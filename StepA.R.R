3a5754c3657179c59efd349ac1e69996ff7b10ef
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 00:22:39 -0400

    Saving StepA.R.R

--- StepA.R.R
+++ StepA.R.R
@@ -6,3 +6,17 @@
 merged_data <- merge(us_arrest, us_census, by.x = "row.names", by.y = c("NAME"), all = TRUE)       # Merge data from us_census and us_arrest got as input from previous     
                                                                                                 # modules using merge function on top of row.name and State Name.
 
+merged_data <- merged_data[-c(9),]
+
+rownames(merged_data) <- seq(length=nrow(merged_data)) 
+
+
+center <- data.frame(state.center)
+area <- data.frame(state.area)
+name <- data.frame(state.name)
+
+as.numeric(rownames(center))
+
+merged_area <- cbind(name, area, center)
+
+merged_data <- merge(merged_data ,merged_area, by.x = c("Row.names"), by.y = c("state.name"), all = TRUE)
