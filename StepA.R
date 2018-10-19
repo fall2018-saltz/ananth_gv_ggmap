780c51bc5f9e91c52af1da59aaea4a06c110eebf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-19 16:41:57 -0400

    Saving StepA.R

--- StepA.R
+++ StepA.R
@@ -1,31 +1,25 @@
 
 
-#3)	Create a merged dataframe -- with the attributes from both dataframes
+#Step A - 	Load and Merge datasets
 
-merged_data <- merge(us_arrest, us_census, by.x = "row.names", by.y = c("NAME"), all = TRUE)       # Merge data from us_census and us_arrest got as input from previous     
-                                                                                                # modules using merge function on top of row.name and State Name.
+merged_data <- merge(us_arrest, us_census, by.x = "row.names", by.y = c("NAME"), all = TRUE)    # merge 3 dataframes together using merge function by "row.names" from arrests and "NAME" (state name) of dfstates
+                                                                                            # Union of data set is created by state name.
 
-merged_data <- merged_data[-c(9),]
+merged_data <- merged_data[-c(9),]       #remove the 9th row which is not a US state.
 
-rownames(merged_data) <- seq(length=nrow(merged_data)) 
+# rownames(merged_data) <- seq(length=nrow(merged_data)) 
 
 
-center <- data.frame(state.center)
-area <- data.frame(state.area)
-name <- data.frame(state.name)
+center <- data.frame(state.center)       # fetch the centre dataset from state data source and convert to data frame using data.frame. This contains 2 columns
+area <- data.frame(state.area)           # fetch the area dataset from state data source and convert to data frame using data.frame
+name <- data.frame(state.name)           # fetch the name dataset from state data source and convert to data frame using data.frame
 
-as.numeric(rownames(center))
+#as.numeric(rownames(center))
 
-merged_area <- cbind(name, area, center)
+merged_area <- cbind(name, area, center) # merge all 3 dataframes name,area,centre. Since they are 50 Objects each they are cross merged by index
 
-merged_data <- merge(merged_data ,merged_area, by.x = c("Row.names"), by.y = c("state.name"), all = TRUE)
+merged_data <- merge(merged_data ,merged_area, by.x = c("Row.names"), by.y = c("state.name"), all = TRUE)   # merge the crime merged data with the merged data frame of centre data by state name.
 
-colnames(merged_data)[1] <- "stateName"
+colnames(merged_data)[1] <- "statename"  # rename the state name in column 1 to statename
 
-library("ggplot2")
-library("ggmap")
-us <- map_data("state")
-options(scipen=999)
-map2 <- ggplot(merged_data, aes(map_id = stateName))
-map2 <- map2 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "black", high = "red")
-map2 <- map2 + expand_limits(x = us$long, y = us$lat)
+merged_data$statename = tolower(merged_data$statename)     # convert all state names to lower case for mapping it against ggmap USA map.
