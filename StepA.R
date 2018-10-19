03baa82ed657195e36bfa525683f18b291291bde
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 21:16:33 -0400

    Saving StepA.R

--- StepA.R
+++ StepA.R
@@ -20,3 +20,12 @@ merged_area <- cbind(name, area, center)
 
 merged_data <- merge(merged_data ,merged_area, by.x = c("Row.names"), by.y = c("state.name"), all = TRUE)
 
+
+library("ggplot2")
+library("ggmap")
+us <- map_data("state")
+options(scipen=999)
+map2 <- ggplot(clean_data_merged, aes(map_id = stateName))
+map2 <- map2 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "black", high = "red")             
+map2 <- map2 + expand_limits(x = us$long, y = us$lat) + coord_map()
+map2
